ed7d8cad97a39486874e6246365245225eb5c816
Ivan Andrey Bolaños Massella <ivanndrey620@gmail.com>
2024-03-06 14:03:43 -0600

    feat: user model created and json_serializable package added

---
+++ lib/features/add_user.dart/domain/models/user.dart
@@ -0,0 +1,30 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user.g.dart';
+
+@JsonSerializable()
+class User {
+  final String name;
+  final String lastname;
+  final int phone;
+  final String addresss;
+  final String email;
+  final String dateOfBirth;
+  final String password;
+  final bool isActive;
+
+  User({
+    required this.name,
+    required this.lastname,
+    required this.phone,
+    required this.addresss,
+    required this.email,
+    required this.dateOfBirth,
+    required this.password,
+    required this.isActive,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+
+  Map<String, dynamic> toJson() => _$UserToJson(this);
+}
--- lib/features/features.dart
+++ lib/features/features.dart
@@ -1,3 +1,4 @@
+export 'package:crud_flutter/features/add_user.dart/domain/models/user.dart';
 export 'package:crud_flutter/app/app.dart';
 export 'package:crud_flutter/features/form/widgets/custom_field_widget.dart';
 export 'package:crud_flutter/features/form/widgets/title_field_widget.dart';
